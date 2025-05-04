9a19f71ab441d3180dfde23c6c5916f67186dd64
Kostromichov Roman <kostromichov.roman@gmail.com>
2025-05-04 14:02:20 +0300

    feat: refactor SwiperList component to remove grid support and enhance item grouping logic

--- resources/js/components/swiper/SwiperList.vue
+++ resources/js/components/swiper/SwiperList.vue
@@ -1,18 +1,15 @@
 <script setup>
 import { ref, computed, onMounted, onUnmounted } from 'vue';
 import { Swiper, SwiperSlide } from 'swiper/vue';
-import { Navigation, Pagination, Grid, Virtual } from 'swiper/modules';
+import { Navigation, Pagination, Virtual } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import 'swiper/css/grid';
 import ButtonBaseWithIcon from '@/components/details/ButtonBaseWithIcon.vue';
-
-const BASE_HEIGHT = 200;
 
 const props = defineProps({
   items: { type: Array, default: [] },
-  rows: { type: Number, default: 1 },
+  rows: { type: Number, default: 2 },
 })
 
 const emit = defineEmits(['reachEnd']);
@@ -25,8 +22,6 @@ const breakpoints = {
   1299: { slidesPerView: 3 },
   1920: { slidesPerView: 4 },
 }
-
-const dynamicHeight = computed(() => `${props.rows * BASE_HEIGHT + 20}px`);
 
 const dynamicWidth = ref(0);
 
@@ -41,6 +36,14 @@ const paginationOptions = {
   renderBullet: (index, className) => `<span class='${className}'>${index + 1}</span>`,
 };
 
+const groupedItems = computed(() => {
+  const result = [];
+  for (let i = 0; i < props.items.length; i += props.rows) {
+    result.push(props.items.slice(i, i + props.rows));
+  }
+  return result;
+});
+
 const emitReachEnd = () => emit('reachEnd');
 
 onMounted(() => {
@@ -52,13 +55,12 @@ onUnmounted(() => {
   window.removeEventListener('resize', updateRows);
 });
 
-
 </script>
 
 <template>
   <swiper
     :key="dynamicWidth"
-    :modules="[ Navigation, Pagination, Grid, Virtual ]"
+    :modules="[ Navigation, Pagination, Virtual ]"
     :slides-per-view="1"
     :space-between="10"
     :pagination="paginationOptions"
@@ -66,14 +68,23 @@ onUnmounted(() => {
     :grab-cursor="true"
     :virtual="true"
     :breakpoints="breakpoints"
-    :grid="{ rows: rows, fill: 'column' }"
     :watch-slides-progress="true"
     class="my_swiper"
     @swiper="(swiper) => swiperRef = swiper"
     @reach-end="emitReachEnd"
   >
-    <swiper-slide v-for="(item, index) in items" :key="index" :virtualIndex="index">
-      <slot :item="item" name="item"></slot>
+    <swiper-slide
+      v-for="(group, index) in groupedItems"
+      :key="index"
+      :virtualIndex="index"
+    >
+      <div
+        v-for="item in group"
+        :key="item.id ?? item"
+        class="slide-item mb-10"
+      >
+        <slot :item="item" name="item" />
+      </div>
     </swiper-slide>
   </swiper>
 </template>
@@ -84,8 +95,9 @@ onUnmounted(() => {
   --swiper-pagination-color: var(--btn-bg-color-inactive);
 
   width: 100%;
-  height: v-bind(dynamicHeight);
+  padding-bottom: 20px
 }
+
 .swiper-pagination-bullet {
   display: inline-block;
 }
