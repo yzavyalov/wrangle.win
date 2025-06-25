98da92b7d7ca6f7f2699fed98b8dfc1159e8e103
Kostromichov Roman <kostromichov.roman@gmail.com>
2025-06-25 20:32:12 +0300

    fix: Update AUTH endpoints to use simplified paths and comment out old URLs

--- resources/js/api/enpoints.js
+++ resources/js/api/enpoints.js
@@ -1,10 +1,13 @@
 export const AUTH = {
-  URL_LOGIN: "/api/login",
+  // URL_LOGIN: "/api/login",
+  // URL_REGISTER: "/api/register",
+  URL_LOGIN: "/login",
+  URL_REGISTER: "/register",
+
   URL_LOGOUT: "/api/logout",
   URL_RESET_PASSWORD: "/api/reset-password",
   URL_FORGOT_PASSWORD: "/api/forgot-password",
   URL_IAFS: "/api/check-user",
-  URL_REGISTER: "/api/register",
   URL_LOGIN_GOOGLE: "/api/auth/google/redirect",
   URL_LOGIN_FACEBOOK: "/api/auth/facebook/redirect",
   URL_LOGIN_FACEBOOK: "/api/auth/telegram/redirect",
