b621d25e04a37c59913ee515be20eb921451f46c
yzavyalov <8540462@gmail.com>
2025-07-30 11:44:24 +0300

    fix url in winteca answer

--- app/Services/Payment/PaymentAnswerService.php
+++ app/Services/Payment/PaymentAnswerService.php
@@ -21,7 +21,7 @@ class PaymentAnswerService
                                 <p style="font-size: 18px;">
                                     Withdrawal of money failed. Please select another method or contact technical support.
                                 </p>
-                                <button onclick="window.location.href= {$this->backUrl}"
+                                <button onclick="window.location.href= '{$this->backUrl}'"
                                         style="padding: 10px 20px; font-size: 16px; background-color: #dc3545; color: white; border: none; border-radius: 5px; cursor: pointer;">
                                     BACK
                                 </button>
@@ -50,7 +50,7 @@ class PaymentAnswerService
                                 <p style="font-size: 16px;">
                                     <strong>Amount:</strong> {$amount} {$currency}
                                 </p>
-                                <button onclick="window.location.href= {$this->backUrl}"
+                                <button onclick="window.location.href= '{$this->backUrl}'"
                                         style="margin-top: 20px; padding: 10px 20px; font-size: 16px; background-color: #28a745; color: white; border: none; border-radius: 5px; cursor: pointer;">
                                     OK
                                 </button>
