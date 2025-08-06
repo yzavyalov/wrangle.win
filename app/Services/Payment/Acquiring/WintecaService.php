b0c394bd66a23b4358ff930acb2f1ca2638746e4
yzavyalov <8540462@gmail.com>
2025-08-06 16:10:05 +0300

    fix:winteca

--- app/Services/Payment/Acquiring/WintecaService.php
+++ app/Services/Payment/Acquiring/WintecaService.php
@@ -67,7 +67,7 @@ class WintecaService
                 'Content-Type' => 'application/json',
             ])->post($url, $payload);
 
-dd($response->json());
+dd(json_encode($payload), $response->json());
             // Проверка на HTTP-ошибку
             if (!$response->successful()) {
                 return false;
