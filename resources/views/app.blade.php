fe25ef1d0317b87499991edbe3bfaca3c822832e
yzavyalov <8540462@gmail.com>
2025-10-01 12:43:23 +0300

    feat:add google tags

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -36,7 +36,15 @@
         <!-- /Yandex.Metrika counter -->
         <script src="//code.jivo.ru/widget/Hc0HvQ3p73" async></script>
 
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-996363155"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
 
+            gtag('config', 'AW-996363155');
+        </script>
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         @inertiaHead
